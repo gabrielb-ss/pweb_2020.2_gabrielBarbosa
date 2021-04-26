63c6e4b7c0f05c18dd48ddb93b48232eb898f337
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2021-04-26 00:32:18 -0300

    Mudei os arquivos do maven de diretorio

---
+++ agropopshop/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+﻿/*Primeira linha em branco - Spring não tava adicionado os valores*/
+INSERT INTO clientes (nome, genero, endereco, cep, profissao, email) VALUES ('Maria da Penha Maia Fernandes', 'feminino', 'rua dos bobos', '1233211-111', 'escritora', 'mariaDaPenha@gov.br');
+INSERT INTO produtos (nome, marca, altura, largura, profundidade, peso, preco) VALUES ('leite em pó', 'nesple', '12cm', '12cm', '12cm', '12g', '12.00');
